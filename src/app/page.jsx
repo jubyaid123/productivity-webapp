37fa6ce939501109a8c7c9f2c2e83e78eed98204
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-09-29 03:40:29 -0400

    change files to jsx and added home page

---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <div className='md:grid md:grid-cols-2 items-center flex flex-col p-5'>
+        <div>
+          <h1 className='head_text text-center'>
+          Manage & Organize
+          <br className='max-md:hidden'></br>
+          <span className='text-center blue_gradient'>All Your Tasks</span>
+          </h1>
+          <p className='desc text-center'>EffiGenius is a task-management and productivity web-application. 
+          We allow our users to manage all their needs in one place, using a calendar and to-do list.</p>
+        </div>
+        <Image className='item-center'
+        src='/assets/home_img1.svg'
+        alt=''
+        width={300}
+        height={300}
+        layout="responsive"
+        />
+      </div>
+    </section>
+  )
+}
+
+export default Home
