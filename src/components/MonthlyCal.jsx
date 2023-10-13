2971e76037f5097f1cacb08851c0b7ff2a3aae31
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-10-13 18:20:58 -0400

    improved css

--- src/components/MonthlyCal.jsx
+++ src/components/MonthlyCal.jsx
@@ -15,24 +15,24 @@ export default function MonthlyCal() {
   const [selectDate, setSelectDate] = useState(currentDate);
 
   return (
-    <div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto h-screen items-center sm:flex-row flex-col">
-      <div className="w-100 h-100">
-        <div className="flex justify between items-center">
+    <div className="mt-10 ml-10 flex gap-10 sm:divide-x justify-center sm:w-3/4 mx-auto h-screen items-center sm:flex-row flex-col">
+      <div className="w-4/5 h-full ml-10">
+        <div className="flex justify-between items-center mb-4 ml-5">
           {/* Display the current month and year */}
-          <h1 className="select-none font-semibold">
+          <h1 className="select-none text-3xl font-semibold">
             {months[today.month()]}, {today.year()}
           </h1>
           <div className="flex gap-10 items-center">
             {/* Navigate to the previous month */}
             <GrFormPrevious
-              className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+              className="w-8 h-8 cursor-pointer hover:scale-105 transition-all"
               onClick={() => {
                 setToday(today.month(today.month() - 1));
               }}
             />
             {/* Go back to the current month */}
             <h1
-              className="cursor-pointer hover:scale-105"
+              className="cursor-pointer hover:scale-105 text-xl"
               onClick={() => {
                 setToday(currentDate);
               }}
@@ -41,7 +41,7 @@ export default function MonthlyCal() {
             </h1>
             {/* Navigate to the next month */}
             <GrFormNext
-              className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+              className="w-8 h-8 cursor-pointer hover:scale-105 transition-all"
               onClick={() => {
                 setToday(today.month(today.month() + 1));
               }}
@@ -53,7 +53,7 @@ export default function MonthlyCal() {
             // Display the days of the week
             <h1
               key={index}
-              className="text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none"
+              className="ml-4 text-xl text-center h-16 w-16 grid place-content-center text-gray-500 select-none"
             >
               {day}
             </h1>
@@ -65,7 +65,7 @@ export default function MonthlyCal() {
             ({ date, currentMonth, today }, index) => (
               <div
                 key={index}
-                className="p-2 text-center h-14 grid place-content-center text-sm border-t">
+                className="p-4 text-center h-16 grid place-content-center text-xl border-t">
                 <h1
                   className={calBool(
                     // Style for current month and selected date
@@ -74,7 +74,7 @@ export default function MonthlyCal() {
                     selectDate.toDate().toDateString() === date.toDate().toDateString()
                       ? "bg-black text-white"
                       : "",
-                    "h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
+                    "h-12 w-12 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
                   )}
                   // Handle date selection
                   onClick={() => {
@@ -87,13 +87,13 @@ export default function MonthlyCal() {
             )
           )}
         </div>
-      </div>
-      <div className="h-80 w-50 sm:px-5">
-        {/* Display the schedule for the selected date */}
-        <h1 className="font-semibold">
+        <div className="mt-10 sm:px-5">
+         {/*Display the schedule for the selected date*/}
+        <h1 className="text-2xl font-semibold">
           Schedule for {selectDate.toDate().toDateString()}
         </h1>
         <p className="text-gray-400">No remaining tasks for today.</p>
+      </div>
       </div>
     </div>
   );
