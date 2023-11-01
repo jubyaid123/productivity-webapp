b8a44873f0ac0e7d05955e04c8792deb1a0643c0
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-11-01 19:18:09 -0400

    new calendar version

--- src/app/calendar/page.jsx
+++ src/app/calendar/page.jsx
@@ -1,17 +1,17 @@
+'use client'
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
 // import MonthlyCal from '../../components/MonthlyCal'
 // import WeeklyCal from '../../components/WeeklyCal'
-import Cal from '../../components/Cal'
+import CalendarComponent from '../../components/calendar'
+
 
 const Calendar = () => {
   return (
     <div className='md:grid-cols-2 calender-placeholder'>
       <Sidebar />
-      <div></div>
-      {/* <MonthlyCal /> */}
-      {/* <WeeklyCal/> */}
-      <Cal/>
+      <div className=''><CalendarComponent/></div>
+      
     </div>
   )
 }
